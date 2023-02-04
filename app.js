9186596c5ba496156501c9a7dfb45759b58dcd90
davidaddi <94124302+davidaddi@users.noreply.github.com>
2023-02-04 11:02:23 +0100

    Gestion triche
    - Impossible de cliquer qu'une seule fois sur le bouton valider après la réponse
    - Impossible de demander la réponse puis de la valider

--- app.js
+++ app.js
@@ -1,64 +1,72 @@
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
-let flag = "Cambodia"; 
+let flag = "Cambodia";
 ans = false;
 answerDisplayed = false
 score = 0;
 
-function getVal() {
-    const inputValue = document.querySelector('input').value;
+//comps 
+const validateBtn = document.querySelector('.validateBtn');
+const skipBtn = document.querySelector('.skip');
+const resultBtn = document.querySelector('.result');
 
-    if(inputValue.toLowerCase() != flag.toLowerCase()) {
-            document.querySelector('.result').classList.add("result-false");
-            document.querySelector('.result').innerHTML = 'Mauvaise Réponse';  
-            document.querySelector('.result').style.color = "red";
-            ans = false;
-           
-    } else {
-        document.querySelector('.result').classList.add("result-true");
-        document.querySelector('.result').innerHTML = 'Bonne Réponse';
-        document.querySelector('.result').style.color = "green";
-        ans = true;
-        score=score+1;
-        document.querySelector('.score').innerHTML = score;
-    }   
+function getVal() {
+  const inputValue = document.querySelector('input').value;
+
+  if (inputValue.toLowerCase() != flag.toLowerCase()) {
+    document.querySelector('.result').classList.add("result-false");
+    document.querySelector('.result').innerHTML = 'Mauvaise Réponse';
+    document.querySelector('.result').style.color = "red";
+  } else {
+    document.querySelector('.result').classList.add("result-true");
+    document.querySelector('.result').innerHTML = 'Bonne Réponse';
+    document.querySelector('.result').style.color = "green";
+    document.querySelector('.skip').innerHTML = 'Drapeau suivant';
+    document.querySelector('.validateBtn').disabled = true;
+    score = score + 1;
+    document.querySelector('.score').innerHTML = score;
+  }
+  validateButton.removeEventListener("click", getVal);
 }
 
 function getData() {
-    var json = 'https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/index.json'
-    fetch(json)
-        .then(data=>data.json())
-        .then(data=> {
-            const randomInt = getRandomInt(data.length);
-            console.log(data[randomInt]);        
-            var image = document.getElementById("flag");
-            image.src = data[randomInt].image;
-            flag = data[randomInt].name;
-        });
-    document.querySelector('.result').innerHTML = '';
-    document.querySelector('.result').innerHTML = '';
+  var json = 'https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/index.json'
+  fetch(json)
+    .then(data => data.json())
+    .then(data => {
+      const randomInt = getRandomInt(data.length);
+      console.log(data[randomInt]);
+      var image = document.getElementById("flag");
+      image.src = data[randomInt].image;
+      flag = data[randomInt].name;
+    });
+  document.querySelector('.result').innerHTML = '';
 }
 
 function getAnswer() {
-    document.querySelector('.result').innerHTML = flag;
+  document.querySelector('.result').innerHTML = flag;
+  document.querySelector('.result').style.color = "white";
+  document.querySelector('.skip').innerHTML = 'Drapeau suivant';
+  // document.querySelector('.validateBtn').disabled = true;
+  document.querySelector('.validateBtn').addEventListener('click', function(){ 
     document.querySelector('.result').style.color = "white";
-    document.querySelector('.next').disabled = true;
-    document.querySelector('.skip').innerHTML = 'Drapeau suivant';
+    document.querySelector('.result').innerHTML = 'Vous ne pouvez pas valider car vous avez demandé la réponse.'});
 }
 
 function skip() {
+  getData();
+  document.querySelector('.skip').innerHTML = 'Je passe';
+  document.querySelector('.validateBtn').disabled = false;
+}
+
+  /* function next() {
+  if (ans == true) {
     getData();
-    document.querySelector('.next').disabled = false;
-    document.querySelector('.skip').innerHTML = 'Je passe';
-}
- 
-function next() {
-    if(ans == true) {
-        getData();
-        inputValue = "";
-    } else {
-        document.querySelector('.result').innerHTML = 'Entrez la bonne réponse';
-    }
-}
+    inputValue = "";
+  } else {
+    document.querySelector('.result').innerHTML = 'Entrez la bonne réponse';
+  }
+  validateButton.addEventListener("click", getVal);
+} */
